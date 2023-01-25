5a3555252f65a6b39f6658c11c10e56220e46c31
charles <ketchasso72@gmail.com>
2023-01-25 20:36:46 +0100

    fetch api of quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Home />} />
+        <Route index path="/quiz" element={<Quiz />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Quiz/Quiz.jsx
+++ src/Pages/Quiz/Quiz.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+import React, { useState, useEffect } from 'react';
 
-export default function Quiz() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const fetchPost = async () => {
+    const response = await fetch(
+      ' https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+    );
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
   return (
-    <div>
-      <p>ready??</p>
+    <div className="App">
+      <p> {posts.category} </p>
+      <button onClick={fetchPost}> get new joke </button>
     </div>
   );
 }
