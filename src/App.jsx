9a57053294f02db4ff9a5015fdba587b577b108a
charles <ketchasso72@gmail.com>
2023-01-26 20:09:29 +0100

    fetch successfully

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Home />} />
-        <Route index path="/quiz" element={<Quiz />} />
+        <Route path="/quiz" element={<Quiz PageNum={0} path="/quiz1" />} />
+        <Route path="/quiz1" element={<Quiz PageNum={1} path="/quiz2" />} />
+        <Route path="/quiz2" element={<Quiz PageNum={2} path="/quiz3" />} />
+        <Route path="/quiz3" element={<Quiz PageNum={3} path="/quiz4" />} />
+        <Route path="/quiz4" element={<Quiz PageNum={4} path="/quiz5" />} />
+        <Route path="/quiz5" element={<Quiz PageNum={5} path="/quiz6" />} />
+        <Route path="/quiz6" element={<Quiz PageNum={6} path="/quiz7" />} />
+        <Route path="/quiz7" element={<Quiz PageNum={7} path="/quiz8" />} />
+        <Route path="/quiz8" element={<Quiz PageNum={8} path="/quiz9" />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Quiz/Quiz.jsx
+++ src/Pages/Quiz/Quiz.jsx
@@ -1,23 +1,36 @@
 /* eslint-disable react/button-has-type */
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Quiz.css';
 
-export default function App() {
+// eslint-disable-next-line react/prop-types
+export default function Quiz({ path, PageNum }) {
   const [posts, setPosts] = useState([]);
   const fetchPost = async () => {
     const response = await fetch(
       ' https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
     );
     const data = await response.json();
-    setPosts(data);
+    setPosts([...data.results]);
   };
-
   useEffect(() => {
     fetchPost();
   }, []);
+
   return (
-    <div className="App">
-      <p> {posts.category} </p>
-      <button onClick={fetchPost}> get new joke </button>
-    </div>
+    posts.length > 0 && (
+      <div className="App">
+        <h1>Question {PageNum}</h1>
+        <h2>Category:{posts[PageNum].category}</h2>
+        <p> {posts[PageNum].question}</p>
+        <input type="radio" />
+        <p>{posts[PageNum].incorrect_answers}</p>
+        <input type="radio" />
+        <p>{posts[PageNum].correct_answers}</p>
+        <Link to={path}>
+          <button type="submit">Next</button>
+        </Link>
+      </div>
+    )
   );
 }
