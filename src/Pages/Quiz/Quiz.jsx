b181c28466d63648f19ee4289e3e3662e034d250
charles <ketchasso72@gmail.com>
2023-02-02 11:15:56 +0100

    react complete project

--- src/Pages/Quiz/Quiz.jsx
+++ src/Pages/Quiz/Quiz.jsx
@@ -6,24 +6,23 @@ import './Quiz.css';
 
 export default function Quiz() {
   const { posts, count, setCount } = useContext(QuizContext);
+  /*   const [disable, setDisable] = useState(false); */
   const navigate = useNavigate();
   const { id } = useParams();
   const page = +id;
+
+  const handleClick = (e) => {
+    const val = e.target.value;
+    if (val === posts[page].correct_answer) {
+      setCount(count + 1);
+    }
+  };
 
   useEffect(() => {
     if (!posts[page]) {
       navigate('/');
     }
   }, []);
-  const handleClick = (e) => {
-    const value = e.target.value;
-    if (value === posts[page].correct_answer) {
-      setCount(count + 1);
-      console.log(`${count}`);
-    } else {
-      console.log(value);
-    }
-  };
 
   return (
     posts.length > 0 && (
@@ -34,25 +33,24 @@ export default function Quiz() {
           <h2>Category:{posts[page].category}</h2>
           <p> {posts[page].question.replace(/([^\w ]|_)/g, '')}</p>
           <div className="boolean">
-            <div className="next_radio">
+            <div className="next_rad">
               <button
-                type="button"
-                className="next"
+                className="Question__btn1"
                 value="True"
-                onClick={(e) => handleClick(e, 'value')}
-              >
-                <b>True</b>
-              </button>
-            </div>
-            <br />
-            <div className="next_radio">
-              <button
+                onClick={(e) => {
+                  handleClick(e, 'val');
+                }}
                 type="button"
-                className="next"
-                value="False"
-                onClick={(e) => handleClick(e, 'value')}
               >
-                <b>False</b>
+                True
+              </button>
+              <button
+                className="Question__btn2"
+                type="button"
+                value="False"
+                onClick={(e) => handleClick(e, 'val')}
+              >
+                False
               </button>
             </div>
           </div>
